6cde5ce6b5ed1b77d21624245a9867b417f2adb6
broyettore <broyettore@gmail.com>
2023-02-20 15:07:18 +0100

    Project scaffolding

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito.
+// Infine scrivi sulla pagina e in console nomecognomecolorepreferito89 (con i valori inseriti dall’utente :wink:)
+// Infine scrivi sulla pagina e in console nomecognomecolorepreferito89 (con i valori inseriti dall’utente :wink:)
+
+"use strict"; 
+
