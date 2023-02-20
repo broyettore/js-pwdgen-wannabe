9339303506081c0849fe9627491b3f91fb10c0e3
broyettore <broyettore@gmail.com>
2023-02-20 17:45:46 +0100

    exercise done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
-// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito.
-// Infine scrivi sulla pagina e in console nomecognomecolorepreferito89 (con i valori inseriti dall’utente :wink:)
-// Infine scrivi sulla pagina e in console nomecognomecolorepreferito89 (con i valori inseriti dall’utente :wink:)
+
 
 "use strict"; 
 
+// Step 1 nome 
+const userName = prompt('Inserisci il tuo nome');
+// Step 2 cognome 
+const userSurname = prompt('Inserisci il tuo cognome');
+// Step 3 colore 
+const userFavoriteColor = prompt('Inserisci il tuo colore preferito');
+
+// step 4 risultato nel body 
+document.querySelector('h1').innerHTML = `Ciao ${userName} ${userSurname}, you favourite color is ${userFavoriteColor}`;
+// step 5 risultato nella console
+console.log(`Ciao ${userName} ${userSurname}, you favourite color is ${userFavoriteColor}`);
